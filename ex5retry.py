5c08d13a55a53d274b1ff741795dd6775773bc33
Kenneth-R <66390879+Kenneth-R@users.noreply.github.com>
2020-06-18 01:13:45 -0400

    Add files via upload

---
+++ ex5retry.py
@@ -0,0 +1,18 @@
+my_name = 'Kenneth G. Roundtree'
+my_age = 17 # just turned
+my_height = 73 # inches
+my_weight = 188 # lbs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} pounds heavy.")
+print(f"Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the tea.")
+
+# this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
